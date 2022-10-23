63c613061d64adaee7099963f2eaed29898ccb0c
cyjbox <cyjbox@gmail.com>
2022-10-23 17:47:57 +0800

    just a test

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="forever git config global user.name 'firstName LastNae'"/>
   </div>
 </template>
 
